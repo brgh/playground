445cce2d02305ca343a27192e2e0e0f6dfbcc9f8
brgh <71097588+brgh@users.noreply.github.com>
2024-08-10 03:52:18 -0700

    Update testScript.js

--- docs/testScript.js
+++ docs/testScript.js
@@ -4,6 +4,8 @@ const checkbox2 = document.querySelector("#cb2");
 const checkbox1Value = document.querySelector("#cb1-value");
 const checkbox2Value = document.querySelector("#cb2-value");
 
+const button = document.querySelector("button"); 
+
 console.log(checkbox1Value.innerText); 
 console.log(checkbox2Value.innerText); 
 
@@ -14,6 +16,14 @@ checkbox1.addEventListener("click", () => {
 checkbox2.addEventListener("click", () => {
   checkbox2Value.innerText = getDateTime(); 
 })
+
+button.addEventListener("click", () => {
+    checkbox1.checked = false;
+    checkbox2.checked = false; 
+    checkbox1Value.innerText = "(placeholder)"; 
+    checkbox2Value.innerText = "(placeholder)";
+    
+  })
 
 function getDateTime() {
   return new Date().toJSON();
